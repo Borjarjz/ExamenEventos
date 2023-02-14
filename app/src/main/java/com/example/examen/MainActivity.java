810b2af70a860b30412e986d232604a4ab80467c
Borja Rodriguez <brodrjim@myuax.com>
2023-02-14 13:42:24 +0100

    examen base. funcional.

--- app/src/main/java/com/example/examen/MainActivity.java
+++ app/src/main/java/com/example/examen/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.examen;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+
+
+
+
+    public void goToSecondActivity(View view) {
+        Intent intent = new Intent(this, ActivityLogin.class);
+        startActivity(intent);
     }
 }
